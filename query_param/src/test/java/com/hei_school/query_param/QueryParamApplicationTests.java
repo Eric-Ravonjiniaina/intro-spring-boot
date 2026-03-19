e663a5a8e902e1945a92ab38925df8324a2e6061
Eric-Ravonjiniaina <rnvjeric@gmail.com>
2026-03-19 23:37:27 +0300

    Initialize project

---
+++ query_param/src/test/java/com/hei_school/query_param/QueryParamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hei_school.query_param;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueryParamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

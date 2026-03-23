2c91913fa2e66fa399067b3433e752a5995e2e56
Eric-Ravonjiniaina <rnvjeric@gmail.com>
2026-03-23 19:58:34 +0300

    chore:modify response StudentController

--- query_param/src/main/java/com/hei_school/query_param/Controller/StudentController.java
+++ query_param/src/main/java/com/hei_school/query_param/Controller/StudentController.java
@@ -1,7 +1,6 @@
 package com.hei_school.query_param.Controller;
 
 import com.hei_school.query_param.Repository.Student;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -10,36 +9,22 @@ import java.util.stream.Collectors;
 
 @RestController
 public class StudentController {
-    private static List<Student> studentList = new ArrayList<>();
+    private static final List<Student> studentList = new ArrayList<>();
 
     @PostMapping("/students")
-    public ResponseEntity<String> addStudents(@RequestBody List<Student> newStudents) {
+    public String addStudents(@RequestBody List<Student> newStudents) {
         studentList.addAll(newStudents);
-        String allNames = studentList.stream()
+
+        return studentList.stream()
                 .map(s -> s.getFirstName() + " " + s.getLastName())
                 .collect(Collectors.joining(", "));
-
-        return ResponseEntity.status(201).body(allNames);
     }
 
     @GetMapping("/students")
-    public ResponseEntity<String> getStudentsByHeader(
-            @RequestHeader(value = "Accept", defaultValue = "text/plain") String acceptHeader) {
-
-        if (acceptHeader.contains("text/plain")) {
-            String result = studentList.stream()
-                    .map(s -> s.getFirstName() + " " + s.getLastName())
-                    .collect(Collectors.joining(", "));
-
-            System.out.println(result);
-
-            return ResponseEntity
-                    .status(200)
-                    .body(result);
-        } else {
-            return ResponseEntity
-                    .status(406)
-                    .body("Format non supporté");
+    public String getStudents(@RequestHeader(value = "Accept") String acceptHeader) {
+        if (acceptHeader.equals("text/plain")) {
+            return studentList.toString();
         }
+        return "Format non supporté";
     }
 }
